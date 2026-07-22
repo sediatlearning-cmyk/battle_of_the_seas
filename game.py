7b034cc27dc4a73db4d075e85c705cdd6010ca59
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-22 17:14:27 +0200

    Add the empty method start() in Game class

--- game.py
+++ game.py
@@ -4,9 +4,10 @@ from dataclasses import dataclass
 @dataclass
 class Game:
     # --------------------Methods------------------------------
+    def start(self):
+        pass
 
     # ----------------------Constructor------------------------
 
     def __init__(self):
         pass
-
