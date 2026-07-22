2c49561c2b0a17a9d924c678e416f9ebd484e5d4
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-22 15:36:34 +0200

    Add board.py file

---
+++ board.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Board:
+
+    # ----------------Methods--------------------
+
+    # ---------------Constructor-----------------
+
+    def __init__(self, row: int, column: int):
+        self.row: int = row
+        self.column: int = column
+
+
+
+
+
+
+
+
