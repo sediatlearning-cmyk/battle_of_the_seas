cb3b97c5319a8049307014967283402bc048af9b
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-22 17:00:09 +0200

    Add game.py file

---
+++ game.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Game:
+    # --------------------Methods------------------------------
+
+    # ----------------------Constructor------------------------
+
+    def __init__(self):
+        pass
+
