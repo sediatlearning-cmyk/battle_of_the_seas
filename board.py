0698cfffad2b5191dd8e07d5f40a695290e1a70e
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-22 17:13:04 +0200

    Add display_board() method in class Board

--- board.py
+++ board.py
@@ -5,6 +5,17 @@ from dataclasses import dataclass
 class Board:
 
     # ----------------Methods--------------------
+    def display_board(self):
+
+        grid = [[" " for _ in range(self.row)] for _ in range(self.column)]
+
+        for row in grid:
+            print("+---" * 10 + "+")
+            for cell in row:
+                print(f"| {cell} |", end="")
+            print("|")
+
+        print("+---" * 10 + "+")
 
     # ---------------Constructor-----------------
 
