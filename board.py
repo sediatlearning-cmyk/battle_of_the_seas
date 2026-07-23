d0627d545ad38013c3281d2ad66170939b4ab67f
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-23 23:29:29 +0200

    update display_board method. To be completed.

--- board.py
+++ board.py
@@ -1,15 +1,16 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class Board:
 
+    row: int = 10
+    column: int = 10
+    grid: list = [[" " for _ in range(row)] for _ in range(column)]
+
     # ----------------Methods--------------------
-    def display_board(self):
 
-        grid = [[" " for _ in range(self.row)] for _ in range(self.column)]
-
+    def display_board(self, grid):
+        letters = [" A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J "]
+        print(letters)
         for row in grid:
+            
             print("+---" * 10 + "+")
             for cell in row:
                 print(f"| {cell} |", end="")
@@ -22,11 +23,3 @@ class Board:
     def __init__(self, row: int, column: int):
         self.row: int = row
         self.column: int = column
-
-
-
-
-
-
-
-
